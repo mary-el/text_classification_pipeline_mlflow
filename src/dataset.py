a3047e9340624543759dc5d4e30fd4fedd9eb366
Maria Piliugina <mary905el@gmail.com>
2025-07-11 07:36:48 +0000

    Added model saving

--- src/dataset.py
+++ src/dataset.py
@@ -2,15 +2,16 @@ import torch
 from torch.utils.data import Dataset
 
 
-
 class TextClassificationDataset(Dataset):
-    def __init__(self, encodings, labels):
+    def __init__(self, encodings, labels, classes):
         self.encodings = encodings
         self.labels = labels
+        self.classes = classes
+        self.label_to_id = {label: i for i, label in enumerate(classes)}
 
     def __getitem__(self, idx):
         item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item["labels"] = torch.tensor(self.labels[idx])
+        item["labels"] = torch.tensor(self.label_to_id[self.labels[idx]])
         return item
 
     def __len__(self):
--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
-from src.load_data import load_data
-from src.train import train_model
-from src.mlflow_logging import setup_mlflow
-from src.utils import load_config
-from src.evaluate import evaluate_model
-from src.model import get_model
-
 import argparse
+
 import mlflow
 from transformers import pipeline
 
+from src.evaluate import evaluate_model
+from src.load_data import load_data
+from src.mlflow_logging import setup_mlflow
+from src.model import get_model, save_model
+from src.train import train_model
+from src.utils import load_config
+
+from dotenv import load_dotenv
+
 
 def main(config_path="configs/params.yaml", eval=False):
+    load_dotenv()
+
     config = load_config(config_path)
     setup_mlflow(config)
 
@@ -22,12 +27,14 @@ def main(config_path="configs/params.yaml", eval=False):
         # Обучение
         if not eval:
             model = train_model(model, train_dataset, val_dataset, config)
+            save_model(model, tokenizer, config)
+            pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
+            # Логирование лучшей модели
+            mlflow.transformers.log_model(pipe, "model")
         # Оценка на тесте
         evaluate_model(model, test_dataset, config)
         # Создание пайплайна
-        pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
-        # Логирование лучшей модели
-        mlflow.transformers.log_model(pipe, "model")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
